bc6430bff0b50a18314f0ccd022109183a7a5543
ton <panupong.pspd@gmail.com>
2018-09-20 09:46:20 +0700

    Initial Commit

---
+++ Factor Funny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Factor Funny
+//
+//  Created by panupong on 20/9/2561 BE.
+//  Copyright © 2561 panupong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
