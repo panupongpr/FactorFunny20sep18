458508a00d3d71265bf8f6c56d3bfd6fae35e850
ton <panupong.pspd@gmail.com>
2018-09-20 11:38:24 +0700

    Have bug

--- Factor Funny/ViewController.swift
+++ Factor Funny/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    Explicit
+    var numberString: String?
+    
+    let timesInt: Int  = 12
+    
+    var factorInt = 2
+    
+    var countInt = 1
+    
+    var resultString = ""
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    
 
+    @IBOutlet weak var numberLabel: UILabel!
+
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        
+        numberString = numberTextField.text!
+        print("numberString ==> \(numberString!)")
+        
+        factorInt = Int(numberString!)!
+        
+        while countInt <= timesInt {
+            
+            resultString = resultString + String(factorInt) + " x " + String(countInt) + "==> " + String(factorInt * countInt) + "\n"
+            countInt += 1
+          
+        }
+          print("result ==> \(resultString)")
+          numberLabel.text = resultString
+    } // calculateButton
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } // ViewDidLoad
+    
 
 
-}
+}  // Main Class
 
